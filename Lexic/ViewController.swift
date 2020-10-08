94a24cc6623cc5d9b940ca1be86d619c839a228c
Harvi <harviandomuhammad@gmail.com>
2020-10-08 15:24:38 +0700

    Initial Commit

---
+++ Lexic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lexic
+//
+//  Created by Muhammad Harviando on 08/10/20.
+//  Copyright © 2020 Muhammad Harviando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
