17943accb4b492f122d8cff9114b360bc225237c
Harvi <harviandomuhammad@gmail.com>
2020-10-15 19:09:08 +0700

    added sound system

---
+++ Lexic/SoundSystem.swift
@@ -0,0 +1,18 @@
+//
+//  SoundSystem.swift
+//  Lexic
+//
+//  Created by Muhammad Harviando on 15/10/20.
+//  Copyright © 2020 Muhammad Harviando. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class SoundSystem: ViewController{
+  var buttonClick: AVAudioPlayer?
+  
+  
+}
+
+
--- Lexic/ViewController.swift
+++ Lexic/ViewController.swift
@@ -8,16 +8,46 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var clickSound:AVAudioPlayer?
+    var refreshedAudio:Bool = false
+    
+    func playClickSound() {
+        guard let url = Bundle.main.url(forResource: "selamat", withExtension: "m4a") else { return }
+        
+        do {
+            clickSound = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.m4a.rawValue)
+            
+            clickSound?.play()
+            
+            if refreshedAudio == false {
+                clickSound?.volume = 0.0
+            } else if (refreshedAudio==true){
+                clickSound?.volume = 0.8
+            }
+            
+        } catch {
+        }
+    }
+    
+    func kickstartAudio(){
+        playClickSound()
+        refreshedAudio = true
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        kickstartAudio()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func buttonPemula(_ sender: Any) {
         self.performSegue(withIdentifier: "pemulaTapped", sender: nil)
+        playClickSound()
     }
     
 }
