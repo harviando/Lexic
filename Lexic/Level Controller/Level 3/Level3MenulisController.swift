5e06a922018ea8e1bc3ce6d4d7449b17001116aa
Harvi <harviandomuhammad@gmail.com>
2020-11-28 15:20:14 +0700

    check point, assigning level controller

---
+++ Lexic/Level Controller/Level 3/Level3MenulisController.swift
@@ -0,0 +1,112 @@
+//
+//  Level1MenulisController.swift
+//  Lexic
+//
+//  Created by Muhammad Harviando on 03/11/20.
+//  Copyright © 2020 Muhammad Harviando. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class Level3MenulisController: UIViewController {
+    var clickSound:AVAudioPlayer?
+    var refreshedAudio:Bool = false
+    
+    @IBOutlet weak var canvasImage1: Canvas!
+    @IBOutlet weak var canvasImage2: Canvas!
+    @IBOutlet weak var canvasImage3: Canvas!
+    @IBOutlet weak var canvasImage4: Canvas!
+    
+    
+    @IBOutlet weak var pencilButton: UIButton!
+    @IBOutlet weak var eraserButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    var pencilActivated = UIImage(named: "toolPencilActive") as UIImage?
+    var pencilNormal = UIImage(named: "toolPencil") as UIImage?
+    
+    var eraserActivated = UIImage(named: "toolEraserActive") as UIImage?
+    var eraserNormal = UIImage(named: "toolEraser") as UIImage?
+    
+    var resetDisabled = UIImage(named: "toolResetDisabled") as UIImage?
+    var resetNormal = UIImage(named: "toolReset") as UIImage?
+    
+    override var shouldAutorotate: Bool{
+        overrideAutoRotate()
+    }
+    
+    func playClickSound() {
+        guard let url = Bundle.main.url(forResource: "buttonClick", withExtension: "mp3") else { return }
+        
+        do {
+            clickSound = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            clickSound?.play()
+            
+            if refreshedAudio == false {
+                clickSound?.volume = 0.0
+            } else if (refreshedAudio==true){
+                clickSound?.volume = 0.8
+            }
+            
+        } catch {
+        }
+    }
+    
+    func kickstartAudio(){
+        playClickSound()
+        refreshedAudio = true
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        forceLandscape()
+        kickstartAudio()
+        
+        pencilButton.setBackgroundImage(pencilActivated, for: .normal)
+        
+        canvasImage1.layer.cornerRadius = 20
+        canvasImage2.layer.cornerRadius = 20
+        canvasImage3.layer.cornerRadius = 20
+        canvasImage4.layer.cornerRadius = 20
+        
+    }
+    
+    @IBAction func toReadingTapped(_ sender: Any) {
+        self.performSegue(withIdentifier: "toReading", sender: nil)
+        playClickSound()
+    }
+    
+    @IBAction func pencilTapped(_ sender: Any) {
+        canvasImage1.pencilMode()
+        canvasImage2.pencilMode()
+        canvasImage3.pencilMode()
+        canvasImage4.pencilMode()
+        pencilButton.setBackgroundImage(pencilActivated, for: .normal)
+        eraserButton.setBackgroundImage(eraserNormal, for: .normal)
+    }
+    @IBAction func eraserTapped(_ sender: Any) {
+        canvasImage1.eraserMode()
+        canvasImage2.eraserMode()
+        canvasImage3.eraserMode()
+        canvasImage4.eraserMode()
+        pencilButton.setBackgroundImage(pencilNormal, for: .normal)
+        eraserButton.setBackgroundImage(eraserActivated, for: .normal)
+    }
+    @IBAction func resetTapped(_ sender: Any) {
+        canvasImage1.image = nil
+        canvasImage2.image = nil
+        canvasImage3.image = nil
+        canvasImage4.image = nil
+        
+    }
+    
+    @IBAction func finishTapped(_ sender: Any) {
+        playClickSound()
+        self.performSegue(withIdentifier: "toReward", sender: nil)
+    }
+    
+}
---
+++ Lexic/Level Controller/Level 3/Level3s4MembacaController.swift
@@ -0,0 +1,87 @@
+//
+//  Level1Controller.swift
+//  Lexic
+//
+//  Created by Muhammad Harviando on 02/11/20.
+//  Copyright © 2020 Muhammad Harviando. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class Level3s4MembacaController: UIViewController {
+    var clickSound:AVAudioPlayer?
+    var refreshedAudio:Bool = false
+    
+    var pencilActivated = UIImage(named: "toolPencilActive") as UIImage?
+    var pencilNormal = UIImage(named: "toolPencil") as UIImage?
+    
+    var eraserActivated = UIImage(named: "toolEraserActive") as UIImage?
+    var eraserNormal = UIImage(named: "toolEraser") as UIImage?
+    
+    var resetDisabled = UIImage(named: "toolResetDisabled") as UIImage?
+    var resetNormal = UIImage(named: "toolReset") as UIImage?
+    
+    override var shouldAutorotate: Bool{
+        overrideAutoRotate()
+    }
+    
+    func playClickSound() {
+        guard let url = Bundle.main.url(forResource: "buttonClick", withExtension: "mp3") else { return }
+        
+        do {
+            clickSound = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            clickSound?.play()
+            
+            if refreshedAudio == false {
+                clickSound?.volume = 0.0
+            } else if (refreshedAudio==true){
+                clickSound?.volume = 0.8
+            }
+            
+        } catch {
+        }
+    }
+    
+    func kickstartAudio(){
+        playClickSound()
+        refreshedAudio = true
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        forceLandscape()
+        kickstartAudio()
+    }
+    
+    func playSpeech() {
+        guard let url = Bundle.main.url(forResource: "Buku", withExtension: "m4a") else { return }
+        
+        do {
+            clickSound = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.m4a.rawValue)
+            
+            clickSound?.play()
+            clickSound?.volume = 2
+            
+        } catch {
+        }
+    }
+    @IBAction func speechTapped(_ sender: Any) {
+        playSpeech()
+    }
+    @IBAction func backToMainTapped(_ sender: Any) {
+        self.performSegue(withIdentifier: "backTapped", sender: nil)
+        playClickSound()
+    }
+    @IBAction func toCanvasTapped(_ sender: Any) {
+        self.performSegue(withIdentifier: "toCanvas", sender: nil)
+        playClickSound()
+    }
+    @IBAction func homeTapped(_ sender: Any) {
+        self.performSegue(withIdentifier: "toHome", sender: nil)
+        playClickSound()
+    }
+}
